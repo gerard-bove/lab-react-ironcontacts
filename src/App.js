5a1ca4184b7ac9e1d968d15033752fb7ec01f518
Gerard Bove <gerardbovetous@gmail.com>
2023-05-03 15:02:15 +0200

    Done!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import jasonContacts from './contacts.json'
 import { useState } from 'react';
 
 function App() {
-  /* let contacts = jasonContacts.slice(0, 5) */
   const [contacts, setContacts] = useState(jasonContacts.slice(0, 5));
   
   const addRandomContacts = () => {
@@ -18,41 +17,43 @@ function App() {
   }
 
   const sortByPopularity = () => {
-    const contactsPopularityCopy = [...contacts].sort((a,b) => a.popularity - b.popularity);
+    const contactsPopularityCopy = [...contacts].sort((a,b) => b.popularity - a.popularity);
     setContacts(contactsPopularityCopy)
   }
 
   const deleteItem = (id)=>{
-    console.log(id)
     setContacts(contacts.filter(contact => contact.id !== id))
-    /* const contactsCopy = [...contacts].filter(contact => contact.id !== id)
-    setContacts(contactsCopy) */
   }
 
   return (
   <div className="App">
-    <button onClick={addRandomContacts}>Add movies</button>
-    <button onClick={sortByName}>Sort by name</button>
-    <button onClick={sortByPopularity}>Sort by popularity</button>
-    <table>
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Popularity</th>
-        <th>Won Oscar</th>
-        <th>Won Emmy</th>
-        <th>Actions</th>
-      </tr>
-      {contacts.map(({pictureUrl, name, popularity, id, wonOscar, wonEmmy}, index) => (
-        <tr key={`contact-${index}`}>
-          <td><img src={pictureUrl} alt="picture"/></td>
-          <td>{name}</td>
-          <td>{popularity}</td>
-          {wonOscar ? <td>🏆</td>:<td></td>}
-          {wonEmmy ? <td>🏆</td>:<td></td>}
-          <td><button onClick={()=>deleteItem(id)}>Delete</button></td>
+    <h1>IronContacts</h1>
+    <button className="button-container" onClick={addRandomContacts}>Add random contact</button>
+    <button className="button-container" onClick={sortByName}>Sort by name</button>
+    <button className="button-container" onClick={sortByPopularity}>Sort by popularity</button>
+    <table className="table-container">
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+          <th>Actions</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {contacts.map(({pictureUrl, name, popularity, id, wonOscar, wonEmmy}, index) => (
+          <tr key={`contact-${index}`}>
+            <td><img className="image-container" src={pictureUrl} alt={name}/></td>
+            <td>{name}</td>
+            <td>{popularity}</td>
+            {wonOscar ? <td>🏆</td>:<td></td>}
+            {wonEmmy ? <td>🏆</td>:<td></td>}
+            <td><button onClick={()=>deleteItem(id)}>Delete</button></td>
+          </tr>
+        ))}
+      </tbody>
     </table>      
   </div>
   );
